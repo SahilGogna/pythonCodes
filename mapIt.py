b5254c07d5da87785b205fd9ba24497829a44e86
Sahil Gogna <gogna.sahil95@gmail.com>
2019-11-26 01:57:13 -0500

    code base upadted

---
+++ mapIt.py
@@ -0,0 +1,12 @@
+#! python3
+# mapIt.py - Launches a map in the browser using an address from the
+# command line or clipboard.
+
+import webbrowser, sys
+
+address =''
+if len(sys.argv) > 1:
+    # Get address from command line.
+    address = ' '.join(sys.argv[1:])
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
---
+++ weatherOutside.py
@@ -0,0 +1,6 @@
+import geocoder, webbrowser
+g = geocoder.ip('me')
+webbrowser.open('https://www.google.com/maps/place/' + g.city)
+
+# refrences https://geocoder.readthedocs.io/api.html -> how to use geo code
+# installing third-Party modules sudo pip3 install ModuleName
