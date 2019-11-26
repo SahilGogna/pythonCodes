6ed1ee21c88832d93adf22187e5276064afbd34a
Sahil Gogna <gogna.sahil95@gmail.com>
2019-11-26 02:08:42 -0500

    code base updated

--- weatherOutside.py
+++ weatherOutside.py
@@ -1,7 +1,7 @@
-#opens current city on google map
+#shows weather of current city, location based on internet provider coordinates
 import geocoder, webbrowser
 g = geocoder.ip('me')
-webbrowser.open('https://www.google.com/maps/place/' + g.city)
+webbrowser.open('https://www.theweathernetwork.com/ca/weather/' +g.state+'/'+ g.city)
 
 # refrences https://geocoder.readthedocs.io/api.html -> how to use geo code
 # installing third-Party modules sudo pip3 install ModuleName
