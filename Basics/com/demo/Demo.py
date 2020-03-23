e36b494eb5bc6597edcbc6e40617f041a41b5ad5
Sahil Gogna <gogna.sahil95@gmail.com>
2020-03-23 15:32:21 -0400

    COVID-19 analysis project added

--- Basics/com/demo/Demo.py
+++ Basics/com/demo/Demo.py
@@ -1,1 +1,8 @@
-print('Hello python ****')
+from com.demo.Employee import Employee
+
+emp1 = Employee("Zara", 2000)
+emp2 = Employee("Manni", 5000)
+
+print(Employee.empCount)
+emp1.displayEmployee()
+emp2.displayEmployee()
